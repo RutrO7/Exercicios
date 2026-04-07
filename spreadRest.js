65153ec1da4e16dc3b4efe99468cdd9172fcdabe
Artur <140997921+RutrO7@users.noreply.github.com>
2026-04-07 07:20:18 -0300

    Aprendendo o Rest operator, + correção de uma sintaxe

--- spreadRest.js
+++ spreadRest.js
@@ -1,21 +1,25 @@
-//O conseito do spreadRest é que, com ele vc consegue duplicar um objeto, ou ate mesmo subscrever parte dele;
+//O conceito do spreadRest é que, com ele vc consegue duplicar um objeto, ou ate mesmo subscrever parte dele;
 // Spread e Rest operator, sintaxe ...;
-const artur = {
-    idade: 19,
-    profissao: 'Consultor',
-    estuda: 'Progamação',
-    possueCNH: 'Simm',
-}
+let artur = {
+  idade: 19,
+  profissao: "Consultor",
+  estuda: "Progamação",
+  possueCNH: "Simm",
+};
 console.log(artur);
 
 // Forma errada de se duplicar um objeto,proposital;
 const artur2 = artur;
 
 // Forma certa de criar outro objeto com os mesmos dados de um ja existente;
-const novoArtur = {... artur};
-    novoArtur.idade = 20;
-    novoArtur.profissao = 'Progamador Junior';
+const novoArtur = { ...artur };
+novoArtur.idade = 20;
+novoArtur.profissao = "Progamador Junior";
 
-console.log('Minha nova profissao é', novoArtur.profissao);
+console.log("Minha nova profissao é", novoArtur.profissao);
 
+// Ja aqui é como usar o Rest operator e pegando um pedaço do codigo😊;
+const { idade, ...resto } = artur;
 
+console.log(novoArtur.idade);
+console.log(resto);
