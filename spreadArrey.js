e4862fc2e336c5788d9e7c9a14ebb4b1fc02d451
Artur <140997921+RutrO7@users.noreply.github.com>
2026-04-12 18:02:34 -0300

    Boas praticas, aprendizagem e correção de bugs

--- spreadArrey.js
+++ spreadArrey.js
@@ -1,16 +1,26 @@
 const listaDeFrutas = ["banana", "manga", "perá"];
 console.log(listaDeFrutas);
 
+//Apenas usando mais o push para add uma coisa a mais na lista;
 listaDeFrutas.push("Pitaya", "Laranja");
 
+//Eu chamei novamente com outro nome, mas contendo as caracteriscitas da lista depois do push;
 const listaDeMaisFrutas = [...listaDeFrutas];
 console.log(listaDeMaisFrutas);
 
+// forma otimizada, porém, está errada na forma de se pegar um determidado parametro, de um arrey;
 console.log(`As frutas que eu gosto são a: ${listaDeFrutas.splice(3)}`);
 
 const soma = [...listaDeFrutas, ...listaDeMaisFrutas];
 console.log(
-  `Minha lista antes de passar no mercado: ${listaDeFrutas}.`,
+  `- Minha lista antes de passar no mercado: ${listaDeFrutas}.`,
   "\n",
-  `Lista atualizada, depois do mercado: ${listaDeMaisFrutas}`,
+  `- Lista atualizada depois do mercado: ${listaDeMaisFrutas}`,
 );
+
+// Da forma certa para poder pegar um pedaço do arrey e dar segmento na palavra;
+const [primeira, segunda, terceira, quarta, quinta] = listaDeMaisFrutas;
+console.log("Minhas frutas favoritas são a:", quarta, "e a", quinta);
+
+// Essa é a diferença e a necessidade de usar o ${} em vez de ',' evita muitass ,,,,;
+console.log(`Minhas frutas favoritas são a: ${quarta} e a ${quinta}`);
