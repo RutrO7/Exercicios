a129449169d392e42f6ba0731ac5d9abd1d9a6ba
Artur <140997921+RutrO7@users.noreply.github.com>
2026-04-14 07:08:23 -0300

    Adicionando mais lógica e conceitos, usando a desconstrução

--- destrucuting.js
+++ destrucuting.js
@@ -3,17 +3,30 @@ const pessoaPassaro = {
   power: 10000,
   nome: "Kaliope",
   idade: 27,
-  sexo: "Masculino",
+  sexo: "masculino",
   horasTrabalhadas: 9,
-
-  
 };
-
 
 console.log(`Nome do pasarro é, ${pessoaPassaro.nome}`);
 
+//Está desconstruindo um objeto utilizando as {};
 const { nome, idade, horasTrabalhadas, power } = pessoaPassaro;
 console.log(
   `- Nome da minha kakatua é ${nome}, ela tem ${idade} anos de idade, 
 com um poder de mais de ${power.toLocaleString(2)} mil e ela geralmete trabalha ${horasTrabalhadas}h por dia.`,
 );
+
+//Da para ficar bem mais legivel, mas, melhor arrumar ou melhor, refatorar, posteriormente;
+//Fazer um função que recebe as caracteristicas de um objeto;
+function caracteristicas({ nome, sexo, idade }) {
+  //Usamos os {}, pois estamos pegando/chamando um pedaço do objeto;
+  console.log("ola", nome);
+  if (idade >= 18) {
+    console.log("Você pode entrar,", nome);
+  } else {
+    console.log("Você não é maior de idade,", nome);
+  }
+  console.log('É um prazer ter um mano,', sexo ,'aqui!');
+}
+// NÃO SE ESQUEÇA DE CHAMAR A FUNÇÃO JAJCUUUUUU
+caracteristicas(pessoaPassaro);
