9f8af4e145515335535d3be7c7859de219495ef2
Artur <140997921+RutrO7@users.noreply.github.com>
2026-04-13 22:14:57 -0300

    Aprendendo a como desconstruir' um pedaço determinado de um obj

---
+++ destrucuting.js
@@ -0,0 +1,18 @@
+const pessoaPassaro = {
+  vida: 5000,
+  power: 10000,
+  nome: "Kaliope",
+  idade: 27,
+  sexo: "Masculino",
+  horasTrabalhadas: 9,
+
+  
+};
+
+console.log(`Nome do pasarro é, ${pessoaPassaro.nome}`);
+
+const { nome, idade, horasTrabalhadas, power } = pessoaPassaro;
+console.log(
+  `- Nome da minha kakatua é ${nome}, ela tem ${idade} anos de idade, 
+com um poder de mais de ${power.toLocaleString(2)} mil e ela geralmete trabalha ${horasTrabalhadas}h por dia.`,
+);
