7873cd4e3dc9ab9f7ce19034297dbff023914ec4
Rutr07 <arturcardoso0001@gmail.com>
2026-04-15 15:56:39 -0300

    Loop e treino

--- exercicios.js
+++ exercicios.js
@@ -8,12 +8,32 @@ for (let i = 0; i <= 10; i++) {
   console.log([i]);
 } // extremamente facil e eu fiz ficar dificil, melhor maneira de executar um arrow sem quebrar a cabeça;
 let test1 = 1;
-if (test1){
-   test1 <= 1
-   console.log('Voce pode entrar', test1)
-} else{
-  console.log('Você não pode')
+if (test1) {
+  test1 <= 1;
+  console.log("Voce pode entrar", test1);
+} else {
+  console.log("Você não pode");
 }
 
-var test =10;
-console.log('Numero é:',test);
+var test = 10;
+console.log("Numero é:", test);
+
+//Uma função com if else basica que compara os numeros e retorna no console
+
+function conta1(i = 0) {
+  if (i <= 10 && i == 0) {
+    console.log("Pode entrar");
+  } else {
+    console.log("Erro");
+  }
+}
+conta1();
+
+//Super simples essa sintax, para percorrer um numero até a parada determinada
+function loop(i = 0) {
+  while (i <= 10) {
+    console.log(`-Seu numero atual é ${i}`);
+    i++;
+  }
+}
+loop();
